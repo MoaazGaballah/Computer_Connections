fd3d3762f2dff8b82baa248746982e22742f90bd
Gaballah Moaaz <gaballah.moaaz@gmail.com>
2020-02-27 01:42:07 +0300

    Initial commit

---
+++ computerConnectionLab/src/lab_24_02_2020/TCP_Client.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab_24_02_2020;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Moaaz
+ */
+public class TCP_Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        String host = "localhost";
+        int port = 44444;
+        
+        new TCP_Client().start(host, port);
+    }
+    
+    private void start(String host, int port) throws IOException{
+        // creating client socket
+        Socket socket = new Socket(host, port);
+        
+        //input: messages comming to client
+        Scanner input = new Scanner(socket.getInputStream());
+        // output : messages going to server
+        PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+        
+        // a message from client's server can be sent
+        System.out.print("Send message to server : ");
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            // read message from console
+            String message = scanner.nextLine();
+            // sending message to server
+            output.println(message);
+            
+            // server closed connection
+            if (message.contains("end") || message.contains("close")) {
+                System.out.println("Client closed the connection!");
+                // terminate the program
+                System.exit(0);
+            }
+            // getting message from server
+            message = input.nextLine();
+            System.out.println("server sends : " + message);
+            
+            // client closed connection
+            if (message.contains("end") || message.contains("close")) {
+                System.out.println("Server closed the connection!");
+                // terminate the program
+                System.exit(0);
+            }
+            
+            System.out.print("Send message to server : ");
+        }
+    }
+    
+}
---
+++ computerConnectionLab/src/lab_24_02_2020/TCP_Server.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab_24_02_2020;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Moaaz
+ */
+public class TCP_Server {
+
+    public static void main(String[] args) throws IOException {
+        int port = 44444;
+
+        new TCP_Server().start(port);
+    }
+
+    private void start(int port) throws IOException {
+        // creating client socket (just port number)
+        ServerSocket socket = new ServerSocket(port);
+        System.out.println("Server launched ..");
+        System.out.println("Please launch a client!");
+
+        while (true) {
+            // blocking call, waiting for new client connection
+            Socket clientSocket = socket.accept();
+
+            System.out.println("New client connected : " + clientSocket);
+            // creating a new thread and make it listening for every client
+            new listenThread(clientSocket).start();
+        }
+    }
+
+    class listenThread extends Thread {
+
+        private final Socket socket;
+        private int clientClosedConnection = 0;
+
+        public listenThread(Socket clientSocket) {
+            this.socket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("New connection established for " + this.getName());
+            try {
+                // the message client sends to server
+                Scanner input = new Scanner(socket.getInputStream());
+                // the message server sends to client
+                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+                // as long as client sends a message
+                while (input.hasNextLine()) {
+                    // client's message
+                    String message = input.nextLine();
+                    System.out.println(this.getName() + " sends : " + message);
+
+                    // if client closes the connection 
+                    if (message.contains("close") || message.contains("end")) {
+                        System.out.println(this.getName() + " closed the connection!");
+                        socket.close();
+                        System.out.println("Socket is closed");
+                        System.out.print("Do you want to wait for another client ?");
+                        Scanner scanner = new Scanner(System.in);
+                        if (scanner.nextLine().contains("no")) {
+                            System.exit(0);
+                        }
+                        clientClosedConnection = 1;
+                        break;
+                    }
+
+                    System.out.print("Please enter a message to " + this.getName() + " : ");
+                    message = new Scanner(System.in).nextLine();
+                    output.println(message);
+                }
+
+            } catch (IOException ex) {
+                Logger.getLogger(TCP_Server.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    if (clientClosedConnection == 0) {
+                        socket.close();
+                        System.out.println("Socket is closed");
+                        System.out.print("Do you want to wait for another client ?");
+                        Scanner scanner = new Scanner(System.in);
+                        if (scanner.nextLine().contains("no")) {
+                            System.exit(0);
+                        }
+                    }                    
+                } catch (IOException ex) {
+                    Logger.getLogger(TCP_Server.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+}
